80b17602945f22dd204105de602dbcbfd59f927a
Miguel Orjuela <ma.orjuela73@gmail.com>
2017-11-23 21:00:18 -0500

    Commit inicial

---
+++ Evaluaciones.R
@@ -0,0 +1,35 @@
+library("tidyverse", lib.loc="~/R/win-library/3.4")
+library("readxl", lib.loc="~/R/win-library/3.4")
+
+pretest_eval1 <- read_excel("../Datos/PRE_TEST_LENGUAJE FIGURADO_EVALUACION 1_Vf.xlsx")
+pretest_eval1 <- pretest_eval1 %>% 
+  unite("Mandragora disfrutaba de una vida sin dolores de cabeza:","Mandragora disfrutaba de una vida sin dolores de cabeza:","INTERPRETACION", sep = "%", remove = TRUE) %>%
+  unite("Mandragora ordeno su habitacion en un abrir y cerrar de ojos:","Mandragora ordeno su habitacion en un abrir y cerrar de ojos:","INTERPRETACION__1", sep = "%", remove = TRUE) %>%
+  unite("Circe pierde la cabeza por un buen postre:","Circe pierde la cabeza por un buen postre:","INTERPRETACION__2", sep = "%", remove = TRUE) %>%
+  unite("Mandragora se quedo con la boca abierta:","Mandragora se quedo con la boca abierta:","INTERPRETACION__3", sep = "%", remove = TRUE) %>%
+  unite("Abriendo los ojos como platos:","Abriendo los ojos como platos:","INTERPRETACION__4", sep = "%", remove = TRUE) %>%
+  unite("Le habian tomado el pelo:","Le habian tomado el pelo:","INTERPRETACION__5", sep = "%", remove = TRUE) %>%
+  unite("Se habia dado con la puerta en las narices:","Se habia dado con la puerta en las narices:","INTERPRETACION__6", sep = "%", remove = TRUE) %>%
+  unite("Gritando a todo pulmon:","Gritando a todo pulmon:","INTERPRETACION__7", sep = "%", remove = TRUE) %>%
+  unite("Las aguas del rio rugian como leones:","Las aguas del rio rugian como leones:","INTERPRETACION__8", sep = "%", remove = TRUE) %>%
+  unite("La bruja con el corazon en la boca:","La bruja con el corazon en la boca:","INTERPRETACION__9", sep = "%", remove = TRUE) %>%
+  unite("Mandragora con los nervios a flor de piel:","Mandragora con los nervios a flor de piel:","INTERPRETACION__10", sep = "%", remove = TRUE) %>%
+  unite("Y asi como por arte de magia:","Y asi como por arte de magia:","INTERPRETACION__11", sep = "%", remove = TRUE) %>%
+  select(-contains("TIPO DE LENGUAJE FIGURADO")) %>% 
+  select(-contains("Marca temporal")) %>% 
+  gather(pregunta,respuesta,c("Mandragora disfrutaba de una vida sin dolores de cabeza:",
+                                                               "Mandragora ordeno su habitacion en un abrir y cerrar de ojos:",
+                                                               "Circe pierde la cabeza por un buen postre:",
+                                                               "Mandragora se quedo con la boca abierta:",
+                                                               "Abriendo los ojos como platos:",
+                                                               "Le habian tomado el pelo:",
+                                                               "Se habia dado con la puerta en las narices:",
+                                                               "Gritando a todo pulmon:",
+                                                               "Las aguas del rio rugian como leones:",
+                                                               "La bruja con el corazon en la boca:",
+                                                               "Mandragora con los nervios a flor de piel:",
+                                                               "Y asi como por arte de magia:")) %>%
+  separate(respuesta, c("respuesta","interpretacion"), sep = "%", remove = TRUE)
+names(pretest_eval1) <- c("nombre","sexo","fecha_nacimiento","grado","pregunta","respuesta","interpretacion")
+ggplot(pretest_eval1,aes(x =pretest_eval1$fecha_nacimiento))+geom_bar()
+
